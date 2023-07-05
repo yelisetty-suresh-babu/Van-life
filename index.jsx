12fd886835614d1575e861b61ff25439cb11fe48
yelisetty-suresh-babu <yelisettysuresh2@gmail.com>
2023-07-05 11:28:25 +0530

    updated the login file

--- index.jsx
+++ index.jsx
@@ -10,20 +10,21 @@ import {
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
-import VanDetail from "./pages/Vans/VanDetail"
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
-import HostVans from "./pages/Host/HostVans"
-import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVans, { loader as hostVansLoader} from "./pages/Host/HostVans"
+import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import NotFound from "./pages/NotFound"
-import Login from "./pages/Login"
+import Login, { loader as loginLoader } from "./pages/Login"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import Error from "./components/Error"
+import { requireAuth } from "./utils"
 
 import "./server"
 
@@ -34,6 +35,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route
       path="login"
       element={<Login />}
+      loader={loginLoader}
     />
     <Route
       path="vans"
@@ -41,17 +43,53 @@ const router = createBrowserRouter(createRoutesFromElements(
       errorElement={<Error />}
       loader={vansLoader}
     />
-    <Route path="vans/:id" element={<VanDetail />} />
+    <Route 
+      path="vans/:id" 
+      element={<VanDetail />} 
+      loader={vanDetailLoader}
+    />
 
     <Route path="host" element={<HostLayout />}>
-      <Route index element={<Dashboard />} />
-      <Route path="income" element={<Income />} />
-      <Route path="reviews" element={<Reviews />} />
-      <Route path="vans" element={<HostVans />} />
-      <Route path="vans/:id" element={<HostVanDetail />}>
-        <Route index element={<HostVanInfo />} />
-        <Route path="pricing" element={<HostVanPricing />} />
-        <Route path="photos" element={<HostVanPhotos />} />
+      <Route
+        index
+        element={<Dashboard />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="income"
+        element={<Income />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="reviews"
+        element={<Reviews />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="vans"
+        element={<HostVans />}
+        loader={hostVansLoader}
+      />
+      <Route
+        path="vans/:id"
+        element={<HostVanDetail />}
+        loader={hostVanDetailLoader}
+      >
+        <Route
+          index
+          element={<HostVanInfo />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="pricing"
+          element={<HostVanPricing />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="photos"
+          element={<HostVanPhotos />}
+          loader={async () => await requireAuth()}
+        />
       </Route>
     </Route>
     <Route path="*" element={<NotFound />} />
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,43 +1,69 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import { loginUser } from "../api";
+
+export function loader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+  const [loginFormData, setLoginFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
+  const message = useLoaderData();
+  const navigate = useNavigate();
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        console.log(loginFormData)
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((data) => {
+        console.log(data);
+        setStatus("idle");
+        navigate("/host", { replace: true });
+      })
+      .catch((err) => setError(err))
+      .finally(() => {
+        setStatus("idle");
+      });
+  }
 
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className="login-form">
-                <input
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Email address"
-                    value={loginFormData.email}
-                />
-                <input
-                    name="password"
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Password"
-                    value={loginFormData.password}
-                />
-                <button>Log in</button>
-            </form>
-        </div>
-    )
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
+  return (
+    <div className="login-container">
+      <h1>Sign in to your account</h1>
+      {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="Email address"
+          value={loginFormData.email}
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={loginFormData.password}
+        />
+        <button disabled={status === "submitting"}>
+          {status === "idle" ? "login" : "logging in...."}
+        </button>
+      </form>
+    </div>
+  );
 }
